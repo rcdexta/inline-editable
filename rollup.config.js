b19eda13600788fba05b753caccea7b8d62c43d3
RC <rc.chandru@gmail.com>
2018-09-25 23:29:02 -0700

    feat(Library): Add Inline-editable textfield, number and select fields

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,42 @@
-import resolve from "rollup-plugin-node-resolve";
-import filesize from "rollup-plugin-filesize";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import pkg from "./package.json";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import cleanup from "rollup-plugin-cleanup";
+import resolve from 'rollup-plugin-node-resolve'
+import filesize from 'rollup-plugin-filesize'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import pkg from './package.json'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import cleanup from 'rollup-plugin-cleanup'
 
-const input = "compiled/index.js";
+const input = 'compiled/index.js'
 
 const plugins = [
   babel(),
   resolve({
-    extensions: [".js", ".jsx", ".json"]
+    extensions: ['.js', '.jsx', '.json']
   }),
-  commonjs(),
+  commonjs({
+    include: 'node_modules/**',
+    namedExports: {
+      'node_modules/styled-components/node_modules/react-is/index.js': ['isValidElementType']
+    }
+  }),
   cleanup(),
   sourceMaps(),
   filesize()
-];
+]
 
 export default [
   {
     input,
-    external: ["react", "react-dom"],
+    external: ['react', 'react-dom'],
     output: [
       {
         file: pkg.unpkg,
-        format: "umd",
+        format: 'umd',
         sourcemap: true,
-        name: "pebble",
+        name: 'pebble',
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM"
+          react: 'React',
+          'react-dom': 'ReactDOM'
         }
       }
     ],
@@ -43,15 +48,15 @@ export default [
     output: [
       {
         file: pkg.module,
-        format: "es",
+        format: 'es',
         sourcemap: true
       },
       {
         file: pkg.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true
       }
     ],
     plugins
   }
-];
+]
