9311fd2c9ecf10e6df3c1e3c4a6ef67c40387bc4
RC <rc.chandru@gmail.com>
2018-09-26 09:45:35 -0700

    fix(Package): Rename package based on availability in npm

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,29 +1,29 @@
-import { configure } from "@storybook/react";
-import { setOptions } from "@storybook/addon-options";
-import "@storybook/addon-console";
+import {configure} from '@storybook/react'
+import {setOptions} from '@storybook/addon-options'
+import '@storybook/addon-console'
 setOptions({
   /**
    * content to display in the top left corner
    * @type {String}
    */
-  name: "inline-editable",
+  name: 'react-inline-components',
   /**
    * URL for content in top left corner to link to
    * @type {String}
    */
-  url: "https://github.com/rcdexta/inline-editable",
+  url: 'https://github.com/rcdexta/inline-editable',
   /**
    * show addon panel as a vertical panel on the right
    * @type {Boolean}
    */
   addonPanelInRight: true
-});
+})
 
 // automatically import all files ending in *.stories.tsx
-const req = require.context("../stories", true, /.stories.tsx$/);
+const req = require.context('../stories', true, /.stories.tsx$/)
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
